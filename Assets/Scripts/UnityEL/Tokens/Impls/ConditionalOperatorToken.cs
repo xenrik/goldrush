272c42c02200fe4323ce64ab6ea85bcde3eec3a4
xenrik <lee@xenrik.com>
2018-12-10 08:48:04 +0000

    Still working on resolving (got interuppted)

--- Assets/Scripts/UnityEL/Tokens/Impls/ConditionalOperatorToken.cs
+++ Assets/Scripts/UnityEL/Tokens/Impls/ConditionalOperatorToken.cs
@@ -1,13 +1,32 @@
-﻿public class ConditionalOperatorToken : RawToken {
+﻿using System.Collections.Generic;
+
+public class ConditionalOperatorToken : RawToken {
     public override string Name { get { return "conditionalOperator"; } }
 
-    public RawToken Test { get; private set; }
-    public RawToken ResultIfTrue { get; private set; }
-    public RawToken ResultIfFalse { get; private set; }
+    public Token Test { get; private set; }
+    public Token ResultIfTrue { get; private set; }
+    public Token ResultIfFalse { get; private set; }
 
     public ConditionalOperatorToken() {
     }
 
     public ConditionalOperatorToken(int position, RawToken parent) : base(position, parent) {
+    }
+
+    public override Token Resolve(Stack<RawToken> rawTokens, Stack<Token> resolvedTokens) {
+        // Take the next raw token as the Test, and the current resolvedToken should be a ConditionalElseToken
+        if (rawTokens.Count == 0 || resolvedTokens.Count == 0) {
+            throw new ParserException(this, "Missing operand");
+        }
+
+        Test = rawTokens.Pop().Resolve(rawTokens, resolvedTokens);
+        Token rhs = resolvedTokens.Pop();
+        if (!(rhs is ConditionalElseToken)) {
+            throw new ParserException(this, "Expected else part of the conditional operator, but found: " + rhs);
+        }
+
+        ConditionalElseToken conditionalElse = (ConditionalElseToken)rhs;
+        ResultIfTrue =
+        return this;
     }
 }
