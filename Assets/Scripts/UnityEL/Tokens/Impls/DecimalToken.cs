ab7c79c901aecc7be41dd964638b99172a85100c
xenrik <lee@xenrik.com>
2018-12-11 08:50:30 +0000

    Adding DecimalParser and Token

---
+++ Assets/Scripts/UnityEL/Tokens/Impls/DecimalToken.cs
@@ -0,0 +1,8 @@
+﻿public class DecimalToken : ValueTokenImpl<float> {
+    public override string Name { get { return "decimal"; } }
+
+    public DecimalToken(float value) : base(value) {
+    }
+    public DecimalToken(float value, int position) : base(value, position) {
+    }
+}
