08d57c8879497012f482fe67876d0481bb378be6
xenrik <lee@xenrik.com>
2019-01-24 12:57:04 +0000

    Working on the new structure of cells/nodes that will allow for rotation

---
+++ Assets/Scripts/Pipe/Cell.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Cell : MonoBehaviour {
+    public enum CellType {
+        EMPTY,
+
+        START,
+        CORNER,
+        END
+    }
+
+    public CellType Type;
+
+    public Vector3Int PreviousOffset;
+    public Vector3Int NextOffset;
+}
